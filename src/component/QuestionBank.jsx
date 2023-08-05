ceabf530ed83f9c44a8f3e1b7c895fb7f8e88177
wasilmughal <mughalwasil928@gmail.com>
2023-08-05 16:04:20 +0500

    first commit

---
+++ src/component/QuestionBank.jsx
@@ -0,0 +1,36 @@
+// QuestionBank.js
+
+const qBank = [
+	{
+		id: 1,
+		question: "What is the capital of pakistan?",
+		options: ["Yamunanagar", "Panipat", "islamabad", "Chandigarh"],
+		answer: "islamabad",
+	},
+	{
+		id: 2,
+		question: "What is the capital of Punjab?",
+		options: ["Patiala", "pindi", "Amritsar", "Chandigarh"],
+		answer: "pindi",
+	},
+	{
+		id: 3,
+		question: "What is the capital of India?",
+		options: ["Delhi", "Mumbai", "Kolkata", "Chennai"],
+		answer: "Delhi"
+	},
+	{
+		id: 4,
+		question: "What is the capital of USA?",
+		options: ["Roorkee", "Haridwar", "London", "Nanital"],
+		answer: "London"
+	},
+	{
+		id: 5,
+		question: "how is Quaid-e-azam",
+		options: ["First Gowrnar jarnal of pakistan", "Lucknow", "Prayagraj", "Agra"],
+		answer: "First Gowrnar jarnal of pakistan"
+	},
+]
+
+export default qBank;
